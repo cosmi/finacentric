8fb13c2e495fe4b88d042046b3faa3fe9e79b784
Marcin Skotniczny <marcin.skotniczny@webservice.pl>
2013-05-22 13:31:17 +0200

    Userzy i admini

--- src/finacentric/routes/admin.clj
+++ src/finacentric/routes/admin.clj
@@ -32,26 +32,12 @@
 ;;     (select-keys params [:first_name :last_name :email])))
 
 
-(defvalidator valid-company
-  (rule :name (<= 5 (count _) 40) "Nazwa musi mieć między 5 a 40 znaków.")
-  (when (not-empty (get-field :domain))
-    (rule :domain (re-matches #"[a-z0-9]+" _) "Domena może się składać wyłącznie z małych liter oraz cyfr")
-    (rule :domain (<= 4 (count _) 30) "Domena musi mieć między 4 a 30 znaków.")))
 
-
-
-(defn companies [page-no & [params]]
-  (layout/render
-    "admin/companies.html" {:companies (korma/select db/companies (db/page page-no 50) (korma/order :id))
-                            :e (get-errors)
-                            :v params}))
-
-;; (defn users [page-no & [{:as params}]]
+;; (defn companies [page-no & [params]]
 ;;   (layout/render
-;;     "admin/users.html" {:users (korma/select db/companies (db/page page-no 50) (korma/order :id))
-;;                         :errors {:domain  (vali/on-error :domain first)
-;;                                  :name (vali/on-error :name first)}
-;;                         :values params}))
+;;     "admin/companies.html" {:companies (korma/select db/companies (db/page page-no 50) (korma/order :id))
+;;                             :e (get-errors)
+;;                             :v params}))
 
 (defn layout [& content]
   (layout/render
@@ -69,20 +55,77 @@
                              [:td (get o k)])
                            [:td
                             (interpose " "
-                                       (for [[k, v] methods]
-                                         (if (-> k name last (= \!))
-                                           [:a {:data-url (str (get o :id)"/"(name k))
-                                                :class "ajaxify" :href "#"} v]
-                                           [:a {:href (str (get o :id)"/"(name k))} v])
+                                       (for [m methods]
+                                         (let [[k,v] (if (fn? m) (m o) m)]
+                                           (let [kname (name k)]
+                                             (if (-> kname last (= \!))
+                                               [:a {:data-url (str (get o :id)"/"(subs kname 0 (dec (count kname))))
+                                                    :class "ajaxify" :href "#"} v]
+                                               [:a {:href (str (get o :id) "/" kname)} v])))
                                          ))]
                            ])]]))
 
+
+
+
 (defn form-wrapper [content]
   (hiccup/html [:form {:method "post"}
-                content
+                [:fieldset content]
                 [:button {:type "submit" :class "btn"} "OK"]]))
-;  <fieldset>
- ;   <legend>Dodawanie domeny </legend>
+
+
+(defmacro object-routes [entity table-fields methods validator entity-form]
+  `(routes
+     (GET "/list" []
+       (with-pagination page-no#
+         (layout
+          (object-table ~table-fields ~methods
+                        (korma/select ~entity (db/page page-no# 50)))
+          (hiccup/html [:a {:href "new"} "Nowy element"])
+                                        ;page-no# (when id (db/select-one ~entity (korma/where {:id id}))))
+          )))
+     (context ["/:id", :id #"[0-9]+"]  {{~'id :id} :params :as request#}
+       (with-integer ~'id
+         (prn :!!id ~'id (-> noir.request/*request* ))
+         (routes
+           ~@(when (not-empty (filter #(when (seq? %) (-> % first (= :delete!) )) methods))
+               [`(POST "/delete" []
+                   (korma/delete ~entity (korma/where {:id ~'id})))])
+
+           ~@(when (not-empty (filter #(when (seq? %) (-> % first (= :edit) )) methods))
+               [`(GET "/edit" []
+                   (prn :IDD ~'id)
+                   (layout
+                    (~entity-form
+                     (db/select-one ~entity (korma/where {:id ~'id})) nil)))
+                `(POST "/edit" {params# :params :as request#}
+                   (if-let [obj# (validates? ~validator params#)]
+                     (and (korma/update ~entity (korma/where {:id ~'id}) (korma/set-fields obj#))
+                          (resp/redirect "list"))
+                     (layout
+                      (~entity-form params# (get-errors)))
+                     ))]))))
+
+     (GET "/new" []
+       (layout
+        (~entity-form nil nil)))
+     (POST "/new" {params# :params :as request#}
+       (if-let [obj# (validates? ~validator params#)]
+         (and (korma/insert ~entity (korma/values [obj#]))
+              (resp/redirect "list"))
+         (layout
+          (~entity-form params# (get-errors)))
+         ))))
+
+
+
+
+
+(defvalidator valid-company
+  (rule :name (<= 5 (count _) 40) "Nazwa musi mieć między 5 a 40 znaków.")
+  (when (not-empty (get-field :domain))
+    (rule :domain (re-matches #"[a-z0-9]+" _) "Domena może się składać wyłącznie z małych liter oraz cyfr")
+    (rule :domain (<= 4 (count _) 30) "Domena musi mieć między 4 a 30 znaków.")))
 
 (defn company-form [input errors]
   (form-wrapper
@@ -93,85 +136,76 @@
        ))))
 
 
+(defvalidator valid-user
+  (rule :first_name (<= 2 (count _) 30) "Imię powinno mieć 2 do 30 znaków")
+  (rule :last_name (<= 2 (count _) 40) "Nazwisko powinno mieć 2 do 40 znaków")
+  (rule :email (vali/is-email? _) "Niepoprawny format adresu email")
+  (rule :email (<= (count _) 50) "Email nie powinien mieć więcej niż 50 znaków"))
 
-(defmacro object-routes [entity table-fields methods validator entity-form]
-  (let [table-fields (eval table-fields)
-        methods (eval methods)]
-    `(routes
-       (GET "/list" []
-         (with-pagination page-no#
-           (layout
-            (object-table ~table-fields ~methods
-                          (korma/select ~entity (db/page page-no# 50)))
-            (hiccup/html [:a {:href "new"} "Nowy element"])
-                                        ;page-no# (when id (db/select-one ~entity (korma/where {:id id}))))
-            )))
-       (context ["/:id", :id #"[0-9]+"]  {{~'id :id} :params :as request#}
-         (with-integer ~'id
-           (prn :!!id ~'id (-> noir.request/*request* ))
-           (routes
-         ~@(when (get methods :delete!)
-             [`(POST "/delete" []
-                   (korma/delete ~entity (korma/where {:id ~'id})))])
+(defn user-form [input errors]
+  (form-wrapper
+   (with-input input
+     (with-errors errors
+       (text-input :first_name "Imię" 30)
+       (text-input :last_name "Nazwisko" 40)
+       (text-input :email "Adres email" 50)
+       ))))
 
-         ~@(when (get methods :edit)
-             [`(GET "/edit" []
-                 (prn :IDD ~'id)
-                 (layout
-                  (~entity-form
-                   (db/select-one ~entity (korma/where {:id ~'id})) nil)))
-              `(POST "/edit" {params# :params :as request#}
-                 (if-let [obj# (validates? ~validator params#)]
-                   (and (korma/update ~entity (korma/where {:id ~'id}) (korma/set-fields obj#))
-                        (resp/redirect "list"))
-                   (layout
-                    (~entity-form params# (get-errors)))
-                   ))]))))
 
-       (GET "/new" []
-         (layout
-          (~entity-form nil nil)))
-       (POST "/new" {params# :params :as request#}
-         (if-let [obj# (validates? ~validator params#)]
-           (and (korma/insert ~entity (korma/values [obj#]))
-                (resp/redirect "list"))
-           (layout
-            (~entity-form params# (get-errors)))
-           )))))
 
 
 
 (defroutes admin-routes
   (context "/admin" {:as request}
-    (GET "/companies" []
-      (with-pagination page-no
-        (with-integer id
-          (companies page-no (when id (db/select-one db/companies (korma/where {:id id}))))
-        )))
+    ;; (GET "/companies" []
+    ;;   (with-pagination page-no
+    ;;     (with-integer id
+    ;;       (companies page-no (when id (db/select-one db/companies (korma/where {:id id}))))
+    ;;     )))
 
-    (POST "/companies/delete" {params :params}
-      (with-integer id
-        (korma/delete db/companies (korma/where {:id id}))
-        ))
-    (context "/cps" []
+    ;; (POST "/companies/delete" {params :params}
+    ;;   (with-integer id
+    ;;     (korma/delete db/companies (korma/where {:id id}))
+    ;;     ))
+    (context "/companies" []
       (object-routes db/companies
                      (array-map :id "#" :name "Nazwa" :domain "Domena")
                      (array-map :delete! "Usuń" :edit "Edytuj")
                      valid-company company-form))
+    (context "/users" []
+      (object-routes db/users
+                     [[:id "#"] [:first_name "Imię"] [:last_name "Nazwisko"] [:email "Email"]]
+                     [[:delete! "Usuń"] [:edit "Edytuj"] #(if-not (% :admin)
+                                                            [:admin-set! "Uczyń adminem"]
+                                                            [:admin-unset! "Nie admin"])]
+                     valid-user user-form)
 
-    (POST "/companies" {params :params}
-      (with-pagination page-no
-        (with-integer id
-          (if-let [company (validates? valid-company params)]
-            (try
-              (do
-                (if id
-                  (db/update-company id company)
-                  (db/create-company company))
-                (resp/redirect (request :uri)))
-              (catch Exception ex
-                (set-error! :name (.getMessage ex))
-                (companies page-no params)))
-            (companies page-no params)
-            ))))))
+     (context ["/:id", :id #"[0-9]+"] [id]
+       (with-integer id
+         (routes
+           (POST "/admin-set" []
+             (korma/update db/users (korma/where {:id id})
+                           (korma/set-fields {:admin true}))
+             )
+           (POST "/admin-unset" []
+             (korma/update db/users (korma/where {:id id})
+                           (korma/set-fields {:admin false}))
+             )
+           ))))))
+
+    ;; (POST "/companies" {params :params}
+    ;;   (with-pagination page-no
+    ;;     (with-integer id
+    ;;       (if-let [company (validates? valid-company params)]
+    ;;         (try
+    ;;           (do
+    ;;             (if id
+    ;;               (db/update-company id company)
+    ;;               (db/create-company company))
+    ;;             (resp/redirect (request :uri)))
+    ;;           (catch Exception ex
+    ;;             (set-error! :name (.getMessage ex))
+    ;;             (companies page-no params)))
+    ;;         (companies page-no params)
+    ;;         ))))))
   
