4c2aca9c61243ea2fa39592c8061474710789e20
Krzysztof Magiera <krzys@zonar.pl>
2013-06-07 17:11:23 +0200

    Validacja nip&regon.

--- src/finacentric/validation_utils.clj
+++ src/finacentric/validation_utils.clj
@@ -1,9 +1,14 @@
 (ns finacentric.validation-utils)
 
+(defn is-nip? [nip]
+  (when (= (count nip) 10)
+    (let [nip (mapv #(-> % str Integer/parseInt) nip)
+          res (reduce + (map * [6 5 7 2 3 4 5 6 7 0] nip))]
+      (= (mod res 11) (get nip 9)))))
 
-(defn is-nip? [s]
-  (re-matches #"[0-9]{10}" s))
+(defn is-regon? [regon]
+  (when (= (count regon) 9)
+    (let [regon (mapv #(-> % str Integer/parseInt) regon)
+          res (reduce + (map * [8 9 2 3 4 5 6 7 0] regon))]
+      (= (mod res 11) (get regon 8)))))
 
-
-(defn is-regon? [s]
-  (re-matches #"[0-9]{9}|[0-9]{14}" s))
