79f959055a2306456dfba2bc74d660e754885135
Krzysztof Magiera <krzys@zonar.pl>
2013-06-13 17:07:00 +0200

    Cleanup.

--- src/finacentric/routes/home.clj
+++ src/finacentric/routes/home.clj
@@ -1,15 +1,9 @@
 (ns finacentric.routes.home
   (:use compojure.core)
   (:require [finacentric.views.layout :as layout]
-            [finacentric.util :as util]))
-
-(defn home-page []
-  (layout/render
-    "home.html" {:content (util/md->html "/md/docs.md")}))
-
-(defn about-page []
-  (layout/render "about.html"))
+            [finacentric.util :as util]
+            [noir.response :as resp]))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/" [] (resp/redirect "/login")))
+
